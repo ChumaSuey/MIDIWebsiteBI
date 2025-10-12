c63b886825ceee0a35c0fc3e477be3f56408ae2b
ChumaSuey <hyperghostblade@gmail.com>
2025-10-11 20:07:31 -0400

    Song added

--- src/components/MIDILibrary.js
+++ src/components/MIDILibrary.js
@@ -9,6 +9,7 @@ const midiFiles = [
   'Blue moon - Blood Instinct.mid',
   'Solis Orientis - Blood Instinct.mid',
   'Berserker Barrage - Blood Instinct.mid',
+  'Lightning Complex - Blood Instinct.mid',
   // the midi file names here (without order as long file name matches).
 ];
 
